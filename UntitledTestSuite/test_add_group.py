7ed56172640dc2ee9e687bc697bb79c8f6d4a205
NatikaReva357 <166649385+NatikaReva357@users.noreply.github.com>
2024-04-12 02:33:14 +0300

    Выделены вспомогательные методы

---
+++ UntitledTestSuite/test_add_group.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+
+class TestAddGroup(unittest.TestCase):
+    def setUp(self):
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
+
+    def test_add_group(self):
+        wd = self.wd
+        self.open_hame_page(wd)
+        self.login(wd)
+        self.open_grups_page(wd)
+        self.create_group(wd)
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def open_hame_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+
+    def login(self, wd):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_grups_page(self, wd):
+        wd.find_element_by_link_text("groups").click()
+
+    def create_group(self, wd):
+        # init group creation
+        wd.find_element_by_name("new").click()
+        # fill group firm
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys("New")
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys("test")
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys("test")
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+
+    def return_to_groups_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def tearDown(self):
+        self.wd.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
