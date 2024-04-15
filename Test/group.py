991c6b34f23520d0e5e3b33bbae0950dde1326c5
NatikaReva357 <166649385+NatikaReva357@users.noreply.github.com>
2024-04-15 22:31:28 +0300

    Выделены вспомогательные методы

---
+++ Test/group.py
@@ -0,0 +1,9 @@
+class Group:
+
+    def __init__(self, firstname, middlename, home, mobile, nickname, lastname):
+        self.firstname=firstname
+        self.middlename = middlename
+        self.home = home
+        self.mobile = mobile
+        self.nickname = nickname
+        self.lastname = lastname
