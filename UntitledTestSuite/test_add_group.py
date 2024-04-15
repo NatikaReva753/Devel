500128fd5d1ecb7c17d28c20b7e2eb4b4b65f4e8
NatikaReva357 <166649385+NatikaReva357@users.noreply.github.com>
2024-04-16 00:23:45 +0300

    Перевели тесты с unittest на py.test и реализовали фикстуры

--- UntitledTestSuite/test_add_group.py
+++ UntitledTestSuite/test_add_group.py
@@ -1,32 +1,21 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
+import pytest
 from group import Group
 from application import Application
-import unittest, time, re
 
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
+@pytest.fixture
+def app(request):
+    fixture = Application
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def test_add_group(self):
-        self.app.login("admin", "secret")
-        self.app.create_group(Group("New", "test", "test"))
-        self.app.logout()
+def test_add_group(app):
+    app.login("admin", "secret")
+    app.create_group(Group("New", "test", "test"))
+    app.logout()
 
-    def test_add_group_two(self):
-        self.app.login("admin", "secret")
-        self.app.create_group(Group("", "", ""))
-        self.app.logout()
-
-    def tearDown(self):
-        self.app.destroy()
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_add_group_two(app):
+    app.login("admin", "secret")
+    app.create_group(Group("", "", ""))
+    app.logout()
