2cc6556a1fe7e236c11e94414ff091957b3fd1a2
NatikaReva753 <166726801+NatikaReva753@users.noreply.github.com>
2024-04-16 20:35:37 +0300

    Update test_add_group.py

--- UntitledTestSuite/test_add_group.py
+++ UntitledTestSuite/test_add_group.py
@@ -7,7 +7,8 @@ from application import Application
 
 @pytest.fixture
 def app(request):
-    fixture = Application
+    fixture = Application()
+
     request.addfinalizer(fixture.destroy)
     return fixture
 
