5d45dab30a13661f915e07a08a3f9df55bb74e70
NatikaReva357 <166649385+NatikaReva357@users.noreply.github.com>
2024-04-16 12:20:00 +0300

    Тесты с unittest переведены на py.test и реализована фикстура

--- Test/HomeWork.py
+++ Test/HomeWork.py
@@ -1,79 +1,20 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 from group import Group
-import unittest, time, re
+from application import Application
+import pytest
 
-class HomeWork(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def test_home_work(self):
-        wd = self.wd
-        self.login(wd, "admin", "secret")
-        self.crete_group(wd, Group("Nata", "test", "test", "test", "test", "test"))
-        self.Logout(wd)
+def test_home_work(app):
+    app.login("admin", "secret")
+    app.crete_group(Group("Nata", "test", "test", "test", "test", "test"))
+    app.Logout()
 
-    def test_home_work_two(self):
-        wd = self.wd
-        self.login(wd, "admin", "secret")
-        self.crete_group(wd, Group("", "", "", "", "", ""))
-        self.Logout(wd)
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/group.php")
-
-    def login(self, wd, username, password):
-        self.open_home_page(wd)
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_new_address_book(self, wd):
-        wd.find_element_by_link_text("add new").click()
-        wd.get("http://localhost/addressbook/edit.php")
-
-    def crete_group(self, wd, group):
-        self.open_new_address_book(wd)
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(group.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(group.middlename)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(group.home)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(group.mobile)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(group.nickname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(group.lastname)
-        wd.find_element_by_xpath("//div[@id='content']/form/input[20]").click()
-        self.return_to_home_page(wd)
-
-    def Logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def return_to_home_page(self, wd):
-        wd.find_element_by_link_text("home page").click()
-        wd.get("http://localhost/addressbook/index.php")
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == "__main__":
-    unittest.main()
+def test_home_work_two(app):
+    app.login("admin", "secret")
+    app.crete_group(Group("", "", "", "", "", ""))
+    app.Logout()
