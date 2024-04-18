4b9c2d8b3ffd50ec3487c294ab33b2944608dba6
NatikaReva357 <166649385+NatikaReva357@users.noreply.github.com>
2024-04-18 20:11:26 +0300

    Сделана общая фикстура для всех тестов

--- UntitledTestSuite/test/test_add_group.py
+++ UntitledTestSuite/test/test_add_group.py
@@ -1,22 +1,12 @@
 # -*- coding: utf-8 -*-
-import pytest
 from UntitledTestSuite.model.group import Group
-from UntitledTestSuite.fixture.application import Application
 
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_group(app):
     app.session.login("admin", "secret")
     app.group.create(Group("New", "test", "test"))
-    app.session.logout()
 
 def test_add_group_two(app):
-    app.session.login("admin", "secret")
     app.group.create(Group("", "", ""))
-    app.session.logout()
+
