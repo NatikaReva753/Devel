ea18b93c9b836c42002950f547b6e50d21b89209
NatikaReva357 <166649385+NatikaReva357@users.noreply.github.com>
2024-04-22 23:54:27 +0300

    Добавлен метод для контакта

--- Test/test/test_add_contact.py
+++ Test/test/test_add_contact.py
@@ -2,9 +2,9 @@
 from Test.model.contact import Contact
 
 def test_add_contact(app):
-    app.session.login("admin", "secret")
+    app.session.login("admin", "secret", "secret")
     app.contact.mycontact(Contact("Nata", "test", "test", "test", "test", "test"))
 
 def test_home_work_two(app):
-    app.session.login("admin", "secret")
+    app.session.login("admin", "secret", "secret")
     app.contact.mycontact(Contact("", "", "", "", "", ""))
--- TestContact/UntitledTestSuite/test_add_contact.py
+++ TestContact/UntitledTestSuite/test_add_contact.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from contact import Contact
 import unittest, time, re
 
 class TestAddContact(unittest.TestCase):
@@ -12,36 +13,64 @@ class TestAddContact(unittest.TestCase):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
     
-    def test_untitled_test_case(self):
+    def test_add_contact(self):
         wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, "admin", "secret")
+        self.open_add_new_contact(wd)
+        self.create_contact(wd, Contact("Nata", "Test", "Test", "Test", "Test"))
+        self.return_to_home_page(wd)
+        self.logout(wd)
+
+    def test_add_contact_two(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, "admin", "secret")
+        self.open_add_new_contact(wd)
+        self.create_contact(wd, Contact("", "", "", "", ""))
+        self.return_to_home_page(wd)
+        self.logout(wd)
+
+    def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
+
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_add_new_contact(self, wd):
         wd.find_element_by_link_text("add new").click()
+    def create_contact(self, wd, contact):
+        # fill contact form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("Nata")
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("Test")
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("Test")
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("Test")
+        wd.find_element_by_name("address").send_keys(contact.address)
         wd.find_element_by_name("work").click()
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys("Test")
+        wd.find_element_by_name("work").send_keys(contact.work)
+        # submit contact creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[20]").click()
+
+    def return_to_home_page(self, wd):
         wd.find_element_by_link_text("home page").click()
+
+    def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
-    
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
