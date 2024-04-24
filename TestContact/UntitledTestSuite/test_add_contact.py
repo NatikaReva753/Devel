e95b7e74a564ec1be3531cd903d223ebffb2932c
NatikaReva357 <166649385+NatikaReva357@users.noreply.github.com>
2024-04-24 22:55:42 +0300

    Переведены тесты на pytest

---
+++ TestContact/UntitledTestSuite/test_add_contact.py
@@ -0,0 +1,19 @@
+from application import Application
+from contact import Contact
+import pytest
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+def test_add_contact(app):
+    app.login("admin", "secret")
+    app.create_contact(Contact("Nata", "Test", "Test", "Test", "Test"))
+    app.logout()
+
+def test_add_contact_two(app):
+    app.login("admin", "secret")
+    app.create_contact(Contact("", "", "", "", ""))
+    app.logout()
