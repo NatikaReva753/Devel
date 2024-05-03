1447a4a6b93caafc81533e9381bbfc9f97e1c0cb
NatikaReva357 <166649385+NatikaReva357@users.noreply.github.com>
2024-05-03 16:28:38 +0300

    Доработки по д/з 7

--- Test/fixture/contact.py
+++ Test/fixture/contact.py
@@ -30,13 +30,25 @@ class ContactHelper:
         wd.find_element_by_xpath("//div[@id='content']/form/input[20]").click()
         self.return_to_home_page()
 
-    def edit_first_contact(self):
+    def edit_first_contact(self, contact):
         self.app.open_home_page()
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
         wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").send_keys("nata")
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(contact.work)
         wd.find_element_by_name("update").click()
         self.return_to_home_page()
 
--- Test/test/test_edit_contact.py
+++ Test/test/test_edit_contact.py
@@ -1,6 +1,6 @@
-
+from Test.model.contact import Contact
 
 
 
 def test_edit_first_contact(app):
-    app.contact.edit_first_contact()
+    app.contact.edit_first_contact(Contact("Nata", "one", "two", "test", "test"))
