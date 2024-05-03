133e9328e794f5a1df783887acdc398e29abe70c
NatikaReva357 <166649385+NatikaReva357@users.noreply.github.com>
2024-05-03 20:32:40 +0300

    Урок 3 д/з 8

--- Test/test/test_edit_contact.py
+++ Test/test/test_edit_contact.py
@@ -3,4 +3,11 @@ from Test.model.contact import Contact
 
 
 def test_edit_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create_contact(Contact(firstname="test", lastname="one", nickname="two", address="test", work="test"))
+    app.contact.edit_first_contact(Contact("", "", "", "", ""))
+
+def test_edit_first_contact_two(app):
+    if app.contact.count() == 0:
+        app.contact.create_contact(Contact(firstname="", lastname="", nickname="", address="", work=""))
     app.contact.edit_first_contact(Contact("Nata", "one", "two", "test", "test"))
