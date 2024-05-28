22bf7204a3bdd81e9efceae6207fd72aa40c5479
NatikaReva357 <166649385+NatikaReva357@users.noreply.github.com>
2024-05-28 08:33:08 +0300

    correct lesson 19

--- Test/test/test_add_contact.py
+++ Test/test/test_add_contact.py
@@ -1,7 +1,7 @@
 from Test.model.contact import Contact
 
 
-def test_add_contact(app, data_contacts):
+def test_add_contacts(app, data_contacts):
     contact = data_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create_contact(contact)
