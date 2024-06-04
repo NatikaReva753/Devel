a6fd9ac25364bf58a1898c6eefdfed0897bd6193
NatikaReva357 <166649385+NatikaReva357@users.noreply.github.com>
2024-06-04 19:18:29 +0300

    Отключаемые проверки пользовательского интерфейса доработка д/з 20

--- Test/test/test_edit_contact.py
+++ Test/test/test_edit_contact.py
@@ -10,14 +10,15 @@ def test_edit_some_contact(app, db, check_ui):
                                            day="", month="", year="", address=""
                                            ))
     old_contacts = db.get_contact_list()
+    modified_contact = random.choice(old_contacts)
     contact = Contact(lastname="test", firstname="Nata",
                       homephone=1234, workphone=2345, mobilephone=3456,
                       email_1="Nata@test.ru", email_2="Nata2@test.ru", email_3="Nata3@test.ru",
                       day="17", month="May", year="2024", address="Moscow")
-    contact.id = random.choice(old_contacts).id
+    contact.id = modified_contact.id
+    app.contact.edit_contact_by_id(modified_contact.id, contact)
     new_contacts = db.get_contact_list()
     #assert len(old_contacts) == len(new_contacts)
-    contact.id = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
     if check_ui:
         assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_group_list(), key=Contact.id_or_max)
--- Test/test/test_modify_group.py
+++ Test/test/test_modify_group.py
@@ -2,14 +2,16 @@ from Test.model.group import Group
 import random
 
 
-def test_modify_group_name(app, db, check_ui):
+def test_modify_group(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="name", header="header", footer="footer"))
     old_groups = db.get_group_list()
+    modified_group = random.choice(old_groups)
     group = Group(name="New name", header="New header", footer="New footer")
-    group.id = random.choice(old_groups).id
+    group.id = modified_group.id
+    app.group.modify_group_by_id(modified_group.id, group)
     new_groups = db.get_group_list()
-    group.id = group
+    assert len(old_groups) == len(new_groups)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
     if check_ui:
         assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
