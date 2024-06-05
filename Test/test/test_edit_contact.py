bf17e62aa497fa910bd746ad0470be25fd374c33
NatikaReva357 <166649385+NatikaReva357@users.noreply.github.com>
2024-06-05 22:50:31 +0300

    correct 20

--- Test/test/test_edit_contact.py
+++ Test/test/test_edit_contact.py
@@ -16,9 +16,12 @@ def test_edit_some_contact(app, db, check_ui):
                       email_1="Nata@test.ru", email_2="Nata2@test.ru", email_3="Nata3@test.ru",
                       day="17", month="May", year="2024", address="Moscow")
     contact.id = modified_contact.id
+    index = old_contacts.index(modified_contact)
     app.contact.edit_contact_by_id(modified_contact.id, contact)
     new_contacts = db.get_contact_list()
-    #assert len(old_contacts) == len(new_contacts)
+    old_contacts[index] = contact
+    assert len(old_contacts) == len(new_contacts)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
     if check_ui:
         assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_group_list(), key=Contact.id_or_max)
 
