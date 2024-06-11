926192cabb608dabf26ba87b04413e2717fe62fa
NatikaReva357 <166649385+NatikaReva357@users.noreply.github.com>
2024-06-11 23:46:31 +0300

    то что удалилось

---
+++ Test Suites/Untitled Test Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Untitled Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>692303ce-1941-4cf1-8292-281c0fba5d7c</testSuiteGuid>
+   <testCaseLink>
+      <guid>709c7fec-1ba2-45bb-96cb-32cde745b01f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Untitled Test Suite/Untitled Test Case</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
+    
---
+++ Test/test/test_add_contact_in_group.py
@@ -0,0 +1,31 @@
+from model.contact import Contact
+from model.group import Group
+import random
+def test_add_contact_in_group(app, orm, check_ui):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="Test",
+                                   middlename="Test",
+                                   lastname="Test",
+                                   nickname="test",
+                                   title="title",
+                                   company="Apple",
+                                   address="Moscow",
+                                   homephone="0000",
+                                   mobilephone="1111",
+                                   workphone="2221",
+                                   fax="123456",
+                                   email_1="Nata1@test.ru",
+                                   email_2="Nata2@test.ru",
+                                   email_3="Nata@test.ru",))
+    if app.group.count() == 0:
+        app.group.create(Group(name='Test'))
+    contacts = orm.get_contact_list()
+    groups = orm.get_group_list()
+    edit_contact = random.choice(contacts)
+    add_group_to_contact = random.choice(groups)
+    app.contact.add_contact_in_group_by_id(edit_contact.id, add_group_to_contact.id)
+    new_contacts = orm.get_contact_list()
+    list_contacts_in_group = orm.get_contacts_in_group(add_group_to_contact)
+    assert edit_contact in list_contacts_in_group
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
